f44bf4ee706684e5f594e3bb8a3b03413e7f7eba
joelbagan <joelbagan@yahoo.com>
2017-02-06 21:07:12 -0800

    Removed redundancies and ensured mGoogleApiClient disconnects immediately after getting a location

--- app/src/main/java/cs48/ucsb/edu/clubhop/MapsActivity.java
+++ app/src/main/java/cs48/ucsb/edu/clubhop/MapsActivity.java
@@ -58,18 +58,8 @@ public class MapsActivity extends FragmentActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment)
-        getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
-                .setInterval(10 * 1000) //10 seconds in ms
-                .setFastestInterval(1 * 1000); //1 seconds, in ms
-
-
-
-
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
@@ -82,7 +72,6 @@ public class MapsActivity extends FragmentActivity implements
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
         mGoogleApiClient.connect();
     }
 
@@ -105,22 +94,6 @@ public class MapsActivity extends FragmentActivity implements
         super.onStop();
     }
 
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            //mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
-
-    private void setUpMap() {
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-    }
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -133,29 +106,7 @@ public class MapsActivity extends FragmentActivity implements
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker on last location and move the camera
-
-        LatLng lastloc;
-        if (mLastLocation != null) {
-            mGoogleApiClient.connect();
-            lastloc = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-            mGoogleApiClient.disconnect();
-
-        } else {
-            lastloc = new LatLng(34.416106, -119.844280);
-        }
-
-        curLocMarker = mMap.addMarker(new MarkerOptions()
-                .position(lastloc)
-                .title("Hello World!")
-                .snippet("I'm a description!! Look at me (>0_0)>")
-        );
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(lastloc)
-                .zoom(17)
-                .build();
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 3000, null);
+        mGoogleApiClient.connect();
         mMap.getUiSettings().setZoomControlsEnabled(true);
     }
 
@@ -174,9 +125,15 @@ public class MapsActivity extends FragmentActivity implements
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (mLastLocation == null) {
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            mLocationRequest = LocationRequest.create()
+                    .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
+                    .setInterval(10 * 1000) //10 seconds in ms
+                    .setFastestInterval(1 * 1000); //1 seconds, in ms
         } else {
             handleNewLocation(mLastLocation);
+            mGoogleApiClient.disconnect();
         }
+
     }
 
     private void handleNewLocation(Location location) {
@@ -185,16 +142,17 @@ public class MapsActivity extends FragmentActivity implements
         double currentLatitude = location.getLatitude();
         double currentLongitude = location.getLongitude();
         LatLng latLng = new LatLng(currentLatitude, currentLongitude);
-
-        if (curLocMarker != null) {
-            curLocMarker.remove();
-        }
-
-        MarkerOptions options = new MarkerOptions()
+        curLocMarker = mMap.addMarker(new MarkerOptions()
                 .position(latLng)
-                .title("I am here!");
-        curLocMarker = mMap.addMarker(options);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                .title("Hi there!")
+                .snippet("handleNewLocation() got called so here I am!")
+        );
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(latLng)
+                .zoom(17)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 2000, null);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
     }
 
     @Override
@@ -233,6 +191,8 @@ public class MapsActivity extends FragmentActivity implements
 
     @Override
     public void onLocationChanged(Location location) {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        mGoogleApiClient.disconnect();
         handleNewLocation(location);
     }
 }
