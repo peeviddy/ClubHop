f37826f613e7cc896e741e4ce975f50b26d23f72
joelbagan <joelbagan@yahoo.com>
2017-02-20 19:46:48 -0800

    added main activity

---
+++ app/src/main/java/cs48/ucsb/edu/clubhop/MainActivity.java
@@ -0,0 +1,160 @@
+package cs48.ucsb.edu.clubhop;
+
+import android.*;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.params.Face;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    TextView textView;
+    CallbackManager callbackManager;
+    UserEventsModel userEventsModel;
+    JSONArray content;
+    String testString;
+
+    final private int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 123;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        setContentView(R.layout.activity_main);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_events");
+        textView = (TextView) findViewById(R.id.textView);
+        callbackManager = CallbackManager.Factory.create();
+        final Intent intent = new Intent(this, cs48.ucsb.edu.clubhop.MapsActivity.class);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                final AccessToken token = AccessToken.getCurrentAccessToken();
+                GraphRequest request = GraphRequest.newMeRequest(
+                        token,
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+
+                                try {
+                                    content = response.getJSONObject().getJSONObject("events").getJSONArray("data");
+                                    while (content == null) {
+                                        textView.setText("null");
+                                    }
+                                    textView.setText("not null");
+                                    userEventsModel = new UserEventsModel( content ); // can't access this outside of the onCompleted
+                                    testString = userEventsModel.getEvent(0).getTitle();
+                                    textView.setText(testString);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+
+                            }
+                        });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "events");
+                request.setParameters(parameters);
+                //request.executeAndWait();
+                request.executeAsync(); // request is not executed immediate which is why userEventsModel gets NullPointerException
+                if ( testString == null ) textView.setText("testString is null");
+                textView.setText(testString);
+
+                /*if (content == null) {
+                    textView.setText("reverted to null");
+                }*/
+
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                textView.setText("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                textView.setText("Login Error");
+            }
+
+        });
+
+        /*if (content == null) {
+            textView.setText("reverted to null");
+        }*/
+
+        //textView.setText( userEventsModel.getEvent(0).getTitle() ); // insta-crash
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void testButton(View view) {
+        Intent intent = new Intent(this, cs48.ucsb.edu.clubhop.MapsActivity.class);
+        startActivity(intent);
+    }
+}
