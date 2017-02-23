87addf3ed1d76fc7e4e60f9dddefded04daac65c
Sean Shelton <seanshelton@umail.ucsb.edu>
2017-02-23 09:53:05 -0800

    Added comments describing the FacebookEvent and the Model.

--- app/src/main/java/cs48/ucsb/edu/clubhop/FacebookEvent.java
+++ app/src/main/java/cs48/ucsb/edu/clubhop/FacebookEvent.java
@@ -4,20 +4,55 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Created by patrick on 2/20/17.
+ * An event that comes from Facebook. It contains many of the different fields that the user may desire.
  */
 
 public class FacebookEvent {
-    //private JSONObject jsonObject;
+
+    /**
+     * The title of the Event.
+     */
     private String title;
+
+    /**
+     * The description of the Event.
+     */
     private String description;
+
+    /**
+     * The Facebook id of this Event.
+     */
     int id;
+
+    /**
+     * The Location of the Event, which contains the coordinates of the Event and the name of the Location.
+     */
     FacebookLocation location;
+
+    /**
+     * The type of the Event. The different types are public, private, community, and group.
+     */
     private String type;
+
+    /**
+     * The time at which the Event begins.
+     */
     private String startTime;
+
+    /**
+     * The time at which the Event ends.
+     */
     private String endTime;
+
+    /**
+     * A URL pertaining to the picture that is set for the whole Event.
+     */
     private String pictureURL;
 
+    /**
+     * The constructor sets the Strings to empty Strings so that we don't try to access
+     * a null String in the Map.
+     */
     FacebookEvent() {
         title = "";
         description = "";
@@ -28,6 +63,10 @@ public class FacebookEvent {
         String pictureURL = "";
     }
 
+    /**
+     * Turns a JSONObject of an Event into a FacebookEvent.
+     * @param eventObject The JSONObject containing all of the information of the Event.
+     */
     public void loadJSONObject(JSONObject eventObject) {
         try {
 
@@ -45,38 +84,73 @@ public class FacebookEvent {
         }
     }
 
+    /**
+     * @return Title of the event.
+     */
     public String getTitle() {
         return checkForNull(title);
     }
 
+    /**
+     * @return Description of the event.
+     */
     public String getDescription() {
         return checkForNull(description);
     }
 
+    /**
+     *
+     * @return FacebookLocation of the Event.
+     */
     public FacebookLocation getLocation() {
         return location;
     }
 
+    /**
+     *
+     * @return Event's id.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     *
+     * @return Event's type.
+     */
     public String getType() {
         return checkForNull(type);
     }
 
+    /**
+     *
+     * @return The time when the Event starts.
+     */
     public String getStartTime() {
         return checkForNull(startTime);
     }
 
+    /**
+     *
+     * @return The time when the Event ends.
+     */
     public String getEndTime() {
         return checkForNull(endTime);
     }
 
+    /**
+     *
+     * @return The URL for the picture of the Event.
+     */
     public String getPictureURL() {
         return checkForNull(pictureURL);
     }
 
+    /**
+     *
+     * @param field A String that could contain something or be null.
+     * @return The original String if it wasn't null. Otherwise, returns an empty String.
+     */
     private String checkForNull(String field) {
         if (field == null)
             return "";
--- app/src/main/java/cs48/ucsb/edu/clubhop/UserEventsModel.java
+++ app/src/main/java/cs48/ucsb/edu/clubhop/UserEventsModel.java
@@ -13,28 +13,49 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 /**
- * Created by patrick on 2/20/17.
+ *  A class that holds all of the FacebookEvents pertaining to a certain User.
+ *
+ *  This class is implemented as a singleton with a static instance, allowing
+ *  any of the other classes in the project to access the Model's data.
  */
 
 public class UserEventsModel {
 
+    /**
+     *  Represents the user that these events pertain to.
+     */
     private User user;
-    private boolean isCreated = false;
 
-    //private ModelListener listener;
-    private ArrayList<ModelListener> listeners = new ArrayList<>();
+    /**
+     * A list of the listeners that are subscribed to the Model.
+     */
+    private ArrayList<ModelListener> listeners;
 
-    //JSONArray events;
-    private ArrayList<FacebookEvent> events = new ArrayList<>();
+    /**
+     * The list of events that pertain to the user.
+     */
+    private ArrayList<FacebookEvent> events;
 
+    /**
+     * The singleton instance of the model.
+     */
     private static UserEventsModel instance;
 
+    /**
+     * @return If the instance is not created yet, it will be created. Else, it returns the instance of the UserEventSModel.
+     */
     public static UserEventsModel getInstance() {
         if (instance == null)
             instance = new UserEventsModel();
         return instance;
     }
 
+
+    /**
+     * Sets the User that the Model pertains to based on the JSON that comes in, which should have the id and name of the User
+     *
+     * @param userJSON The JSONObject that has the information of the User (namely "id" and "name")
+     */
     public void setUser(JSONObject userJSON) {
         try {
             user = new User(userJSON.getInt("id"), userJSON.getString("name"));
@@ -43,6 +64,10 @@ public class UserEventsModel {
         }
     }
 
+    /**
+     * Takes in a JSONArray of events, turns them into FacebookEvents, and then adds them to the Model.
+     * @param eventArray
+     */
     public void loadJSONArray(JSONArray eventArray) {
         for (int i = 0; i < eventArray.length(); ++i) {
             try {
@@ -58,23 +83,40 @@ public class UserEventsModel {
         notifyListeners();
     }
 
+    /**
+     * Private constructor that initializes listeners and events to be default ArrayLists.
+     */
     private UserEventsModel() {
-
+        listeners = new ArrayList<>();
+        events = new ArrayList<>();
     }
 
-
+    /**
+     * Returns a single event from within the Model
+     * @param index The index for the desired FacebookEvent.
+     * @return FacebookEvent at the given index inside of the Model.
+     */
     public FacebookEvent getEvent(int index) {
         return events.get(index);
     }
 
+    /**
+     * @return The size of the list of FacebookEvents.
+     */
     public int getSize() {
         return events.size();
     }
 
-    public boolean isCreated() { return isCreated; }
-
+    /**
+     * Subscribes a listener to this Model, which will be notified when the Model changes.
+     *
+     * @param listener A new listener that would like to subscribe to the model.
+     */
     public void addListener(ModelListener listener) { this.listeners.add(listener); }
 
+    /**
+     * Tells all of the subscribed listeners that the model has changed.
+     */
     private void notifyListeners() {
         for (int i = 0; i < listeners.size(); ++i) {
             listeners.get(i).onChange();
