08cfe264cd87efbdfa0e3b8d373316af4f24d49d
joelbagan <joelbagan@yahoo.com>
2017-02-24 13:51:59 -0800

    renamed UserEventsModelListener class

--- app/src/main/java/cs48/ucsb/edu/clubhop/UserEventsModelListener.java
+++ app/src/main/java/cs48/ucsb/edu/clubhop/UserEventsModelListener.java
@@ -4,7 +4,7 @@ package cs48.ucsb.edu.clubhop;
  * A listener that will be notified when the model changes.
  */
 // Maybe refactor this into just a general listener?
-public interface ModelListener {
+public interface UserEventsModelListener {
     public void onEventsCreated();
     public void onMarkersCreated();
 }
