f5b39fd10b04d044d57f4da7c317054e969c761d
Bryanna Phan <bryannaphan@Bryannas-MacBook-Air.local>
2017-02-24 02:09:07 -0800

    trying to settle merge conflict w/ ModelListener.java vs. UserEventModelListener.java

---
+++ app/src/main/java/cs48/ucsb/edu/clubhop/ModelListener.java
@@ -0,0 +1,13 @@
+package cs48.ucsb.edu.clubhop;
+
+/**
+ * A listener that will be notified when the model changes.
+ */
+<<<<<<< HEAD:app/src/main/java/cs48/ucsb/edu/clubhop/UserEventsModelListener.java
+public interface ModelListener {
+=======
+// Maybe refactor this into just a general listener?
+public interface ModelListener {
+>>>>>>> 1eab30bd1393d452d62f41fc85e5862567a7b013:app/src/main/java/cs48/ucsb/edu/clubhop/ModelListener.java
+    public void onChange();
+}
